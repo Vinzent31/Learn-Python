a08e3c4f525c0a61f9e30e4a9c02b2f61fb51bb9
Vinzent31 <vinzentgabriel60@gmail.com>
2026-09-23 09:52:17 +0700

    Multiple Assignment

---
+++ multiple_assignment.py
@@ -0,0 +1,14 @@
+#Multiple assignment
+
+name, age, human = "Vinzent", 22, True
+
+print(name)
+print(age)
+print(human)
+
+Vinzent = Syafiq = ardan = rasya = 17
+
+print(Vinzent)
+print(ardan)
+print(rasya)
+print(Syafiq)
--- variable.py
+++ variable.py
@@ -1,28 +1,30 @@
 # Learning Variable
-first_name = "Vinzent"
-last_name = "Gabriel"
-full_name = first_name + " " + last_name
+# first_name = "Vinzent"
+# last_name = "Gabriel"
+# full_name = first_name + " " + last_name
 #print("Hello " + full_name)
 
 #Check the type of variable
 #print(type(full_name))
 
-#Making variable int data type & math operations
-age = 13
-age += 4
-# print(age)
+# #Making variable int data type & math operations
+# age = 13
+# age += 4
+# # print(age)
+#
+# #Making float data type variable
+# height = 168.3
+# weight = 68
+#
+# #Boolean Data Type
+# human = True
+#
+#
+# print("Hello! My name is " + full_name + "!")
+# print("I'm " + str(age) + " Years old")
+# print("My Height is " + str(height) + " CM")
+# print("My Weight is " + str(weight) + " KG")
+# print("Am i Human: " + str(human))
 
-#Making float data type variable
-height = 168.3
-weight = 68
 
-#Boolean Data Type
-human = True
-
-
-print("Hello! My name is " + full_name + "!")
-print("I'm " + str(age) + " Years old")
-print("My Height is " + str(height) + " CM")
-print("My Weight is " + str(weight) + " KG")
-print("Am i Human: " + str(human))
 

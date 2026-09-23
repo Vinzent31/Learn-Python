43eef814ed7515e7404b196fbee9f107c08959fc
Vinzent31 <vinzentgabriel60@gmail.com>
2026-09-23 12:53:16 +0700

    String Method

---
+++ string_method.py
@@ -0,0 +1,11 @@
+name = "vinzent"
+
+print(len(name))
+print(name.find("z"))
+print(name.capitalize())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("n"))
+print(name.replace("n", "a"))
+print(name*3)

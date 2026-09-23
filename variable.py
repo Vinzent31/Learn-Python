5f66c517471300ca0c03928ce469b1a3212be3ea
Vinzent31 <vinzentgabriel60@gmail.com>
2026-09-23 08:55:51 +0700

    First Commit

---
+++ variable.py
@@ -0,0 +1,28 @@
+# Learning Variable
+first_name = "Vinzent"
+last_name = "Gabriel"
+full_name = first_name + " " + last_name
+#print("Hello " + full_name)
+
+#Check the type of variable
+#print(type(full_name))
+
+#Making variable int data type & math operations
+age = 13
+age += 4
+# print(age)
+
+#Making float data type variable
+height = 168.3
+weight = 68
+
+#Boolean Data Type
+human = True
+
+
+print("Hello! My name is " + full_name + "!")
+print("I'm " + str(age) + " Years old")
+print("My Height is " + str(height) + " CM")
+print("My Weight is " + str(weight) + " KG")
+print("Am i Human: " + str(human))
+

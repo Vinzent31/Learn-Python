a03aa497587a4e9b6dba2b18d71850da4d874251
Vinzent31 <vinzentgabriel60@gmail.com>
2026-09-24 11:27:29 +0700

    Type Cast

---
+++ typecast.py
@@ -0,0 +1,21 @@
+#Type Cast = convert data type to other data type
+
+x = 1
+y = 2.0
+z = "3"
+a = 9
+b = 6.4
+
+a = float(a)
+b = str(b)
+y = int(y)
+z = int(z)
+add = z + x
+
+print (x)
+print (y)
+print (z)
+print (a)
+print (type(b))
+
+print(add)
